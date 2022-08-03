5ac889b96fc5833e50a844f6e90eb68e2e57acd5
ProtosL <644074553@qq.com>
2022-08-03 15:55:40 +0800

    4.1.1 错误处理

--- lox.ts
+++ lox.ts
@@ -2,6 +2,8 @@ import readline from 'readline';
 import fs from 'fs';
 
 class Lox {
+    static hasError = false;
+    
     public static main(args: string[]) {
         if (args.length > 1) {
             console.log("Usage: jlox [script]");
@@ -16,6 +18,10 @@ class Lox {
     private static runFile(path: string) {
         const data = fs.readFileSync(path, { encoding: 'utf-8' });
         this.run(data);
+
+        if (this.hasError) {
+            process.exit(1);
+        }
     }
 
     private static runPrompt() {
@@ -30,6 +36,9 @@ class Lox {
                 return ;
             }
             this.run(name)
+
+            // 重置 hasError 标志，交互式循环中用户出错不应该终止他们的会话
+            this.hasError = false;
         })
     }
 
@@ -41,5 +50,14 @@ class Lox {
         // tokens.forEach(token => {
         //     console.log(token);
         // })
+    }
+
+    static error(line: number, message: string) {
+        this.report(line, '', message);
+    }
+
+    private static report(line: number, where: string, message: string) {
+        console.error("[line " + line + "] Error" + where + ": " + message);
+        this.hasError = true;
     }
 }
